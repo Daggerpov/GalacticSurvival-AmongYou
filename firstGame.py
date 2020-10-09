dd7cae014d66a5fc403a182a9b792de6d3a8d00e
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-09 13:48:05 -0400

    Update firstGame.py

--- firstGame.py
+++ firstGame.py
@@ -1,27 +1,28 @@
 import pygame
 pygame.init()
 
-GUI = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("Galactic Survival: Among You")
+GUI = pygame.display.set_mode((500, 500)) #Makes Window
+pygame.display.set_caption("Galactic Survival: Among You") #Title of Window
 
 green = (0, 255, 0)
 count = 0
-font = pygame.font.SysFont('arial', 32)
-posText = (0, 0)
+font = pygame.font.SysFont('arial', 32) #Font for text
+posText = (0, 0) 
 running = "True"
+#Main Loop
 while running:
-    pygame.time.delay(75)
-    for event in pygame.event.get():
-        #if event.type == pygame.MOUSEBUTTONDOWN:
+    pygame.time.delay(75) 
+    for event in pygame.event.get(): #Allows events/actions from mouse/keyboard
+        #if event.type == pygame.MOUSEBUTTONDOWN: #Testing Mouse-Clicked
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT: #Testing out left arrow key
                 count-=1 
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT: #Testing out right arrow key
                 count+=1 
-        GUI.fill((0, 0, 0))
-        text = font.render('Count = ' + str(count), True, green)
-        GUI.blit(text, posText)
-        if event.type == pygame.QUIT:
+        GUI.fill((0, 0, 0)) #The background of the display window
+        text = font.render('Count = ' + str(count), True, green) #Initializing text
+        GUI.blit(text, posText) #Printing text on screen, requires text and position
+        if event.type == pygame.QUIT: #The close window function
             running = False
-    pygame.display.update()
+    pygame.display.update() #Updates the screen
 pygame.quit()
