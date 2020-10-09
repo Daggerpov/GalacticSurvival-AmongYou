f8cd0b91b5141b1930ea6272bfcd7c2079816eef
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-09 08:55:23 -0400

    Update firstGame.py

--- firstGame.py
+++ firstGame.py
@@ -6,14 +6,17 @@ pygame.display.set_caption("First Game")
 
 green = (0, 255, 0)
 blue = (0, 0, 128)
-font = pygame.font.Font('freesansbold.ttf', 32)
-text = font.render('Mumbo Jumbo', True, green)
-text.center = (500//2, 500//2)
+count = 0
+font = pygame.font.Font('freesansbold.ttf', 16)
+posText = (0, 0)
 running = "True"
 while running:
     pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
+            count+=1
+            GUI.fill((0, 0, 0))
+            text = font.render('Count = ' + str(count), True, green)
             GUI.blit(text, posText)
             pygame.display.update()
         if event.type == pygame.QUIT:
