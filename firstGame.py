9c802022e64ceb0355ad07fe28e4b10606364530
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-09 10:23:48 -0400

    Update firstGame.py

--- firstGame.py
+++ firstGame.py
@@ -5,19 +5,22 @@ GUI = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Galactic Survival: Among You")
 
 green = (0, 255, 0)
-blue = (0, 0, 128)
 count = 0
-font = pygame.font.Font('freesansbold.ttf', 16)
+font = pygame.font.SysFont('arial', 32)
 posText = (0, 0)
 running = "True"
 while running:
-    pygame.time.delay(1000)
+    pygame.time.delay(75)
     for event in pygame.event.get():
-        count+=1
+        #if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                count-=1 
+            elif event.key == pygame.K_RIGHT:
+                count+=1 
         GUI.fill((0, 0, 0))
         text = font.render('Count = ' + str(count), True, green)
         GUI.blit(text, posText)
-        #if event.type == pygame.MOUSEBUTTONDOWN: 
         if event.type == pygame.QUIT:
             running = False
     pygame.display.update()
