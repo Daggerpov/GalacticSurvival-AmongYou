c319256d2d7b884324642af97f867801f14beee0
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-09 07:59:16 -0400

    Update firstGame.py

--- firstGame.py
+++ firstGame.py
@@ -1,6 +1,13 @@
 import pygame
 pygame.init()
 
-GUI = pygame.display.set_mode((500, 500))
-
+win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First Game")
+
+running = "True"
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
