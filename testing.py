be50ac93b3a3683cecfe9e12c0e6e8847eeba1c2
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-17 14:33:08 -0400

    Made a movable object using arrow keys, in testing.py

--- testing.py
+++ testing.py
@@ -3,33 +3,41 @@ import os
 pygame.init() #Initializes the PyGame module
 
 GUI = pygame.display.set_mode((500, 500)) #Makes Window
-pygame.display.set_caption("Galactic Survival: Among You") #Title of Window
+pygame.display.set_caption("Testing") #Title of Window
 
 #resources = pygame.font.get_fonts()
 #title = pygame.image.load(os.path.join('data', 'GalacticSurvivalTitle.png'))
 green = (0, 255, 0)
 count = 0
 
+x = 500/6
+y = 500/6
+
 #When inserting PATH for new fonts, add 'r' infront to convert form string to raw string
-font = pygame.font.Font(r'C:\Users\Sanjeev\Documents\GitHub\ICS3U-firstGame\venv\Lib\site-packages\pygame\In your face, joffrey!.ttf', 69) #Font for text
+font = pygame.font.Font('freesansbold.ttf', 69) #Font for text
 posText = (0, 0) 
 running = "True"
 
 while running:
     pygame.time.delay(75) 
     for event in pygame.event.get(): #Allows events/actions from mouse/keyboard
-        #if event.type == pygame.MOUSEBUTTONDOWN: #Testing Mouse-Clicked
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT: #Testing out left arrow key
-                count-=1 
-            elif event.key == pygame.K_RIGHT: #Testing out right arrow key
-                count+=1 
-        GUI.fill((0, 0, 0)) #The background of the display window
-        text = font.render('Count = ' + str(count), True, green) #Initializing text
-        GUI.blit(text, posText) #Printing text on screen, requires text and position
-        #pygame.mixer.music.load('jazz.wav')
-        #pygame.mixer.music.play(-1)
         if event.type == pygame.QUIT: #The close window function
             running = False
+        #if event.type == pygame.MOUSEBUTTONDOWN: #Testing Mouse-Clicked
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x -= 10
+            if event.key == pygame.K_RIGHT:
+                x += 10
+            if event.key == pygame.K_UP:
+                y -= 10
+            if event.key == pygame.K_DOWN:
+                y += 10
+        
+    GUI.fill((0, 0, 0))   
+
+    pygame.draw.rect(GUI, green,[x, y, 20, 20])
+        
     pygame.display.update() #Updates the screen
 pygame.quit()
+quit()
