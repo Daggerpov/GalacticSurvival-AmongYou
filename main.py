85615dbd4c9c762d896f27835fc0fa6eefa461c1
Daggerpov <danielagapov1@gmail.com>
2020-10-18 13:22:02 -0400

    changed instructions font to the riven

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ running = True
 titleFont = pygame.font.Font(r'./in-your-face-joffrey/In your face, joffrey!.ttf', 100)
 buttonFont = pygame.font.Font(r'./in-your-face-joffrey/In your face, joffrey!.ttf', 32)
 taskFont = pygame.font.Font(r'./in-your-face-joffrey/In your face, joffrey!.ttf', 50)
-instructFont = pygame.font.Font(r'./in-your-face-joffrey/In your face, joffrey!.ttf', 36)
+instructFont = pygame.font.Font(r'./the-riven/The Riven.ttf', 36)
 
 def posText(text, x, y):
     titleRect = text.get_rect()
