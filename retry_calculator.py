e09d0934dc7abb79ba923681a320ead1885d1a5a
Daggerpov <danielagapov1@gmail.com>
2020-10-20 17:28:54 -0400

    Create retry_calculator.py

---
+++ retry_calculator.py
@@ -0,0 +1,24 @@
+def retry_calculator(index):
+    import main
+    from main import lose, index, short_tasks, long_tasks
+    import math
+    
+    multiplier = 1
+
+    if index == 0:
+        multiplier = 2
+    elif index == 1:
+        multiplier = 1
+    elif index == 2:
+        multiplier = 0.5
+    elif index == 3:
+        multiplier = 0
+
+    retry_amount = math.floor((multiplier * short_tasks) + (multiplier * long_tasks * 2))
+
+    if retry_amount >= 0:
+        lose = False
+        return retry_amount
+    else:
+        lose = True
+        end() 
