2653852dbf8986b028e7169207dad05af6674d18
Daggerpov <danielagapov1@gmail.com>
2020-10-21 12:18:39 -0400

    Update endMenu.py

--- endMenu.py
+++ endMenu.py
@@ -2,7 +2,6 @@ def end():
     import main
     from main import gui, width, height, titleFont, buttonFont, mouse, pygame, posText, spacebg
 
-
     green = (0, 255, 0)
     red = (255, 0, 0)
     dark_red= (200, 0, 0)
@@ -11,12 +10,16 @@ def end():
     gui.fill((0, 0, 0))
     spacebg()
 
+    #renders text for button
     menuButton = buttonFont.render("Return to Main Menu", True, blue)
 
-    for event in pygame.event.get(): #Allows events/actions from mouse/keyboard
+    #Allows events/actions from mouse/keyboard
+    for event in pygame.event.get(): 
+        #The close window function
         if event.type == pygame.QUIT:
            quit()
-        if event.type == pygame.MOUSEBUTTONDOWN: #The close window function
+        #checks if mouse clicks on the button
+        if event.type == pygame.MOUSEBUTTONDOWN: 
             if width/2-150 <= mouse[0] <= width/2+150 and height/4*3 <= mouse[1] <= height/4*3+40:
                 main.screen = 0
                 main.lose = False 
@@ -28,7 +31,8 @@ def end():
                 main.fails = 0
                 
     
-
+    #if lost then it randomizes between the 5 different loss messages and renders the text
+    #otherwise, it'll just render the default win message
     if main.lose == True:
         messages = [
             f"{main.username}, you've doomed us all!", \
@@ -41,13 +45,15 @@ def end():
     else:
         endTitle = titleFont.render("You've saved the ship!", True, green)
 
+    #displays text
     gui.blit(endTitle, posText(endTitle, width//2, 75))
 
-
+    #lights up button when hovering over
     if width/2-150 <= mouse[0] <= width/2+150 and height/4*3 <= mouse[1] <= height/4*3+40: 
         pygame.draw.rect(gui, red,[width/2-150, height/4*3, 300, 40]) 
         
     else: 
         pygame.draw.rect(gui, dark_red, [width/2-150, height/4*3, 300, 40]) 
 
+    #displays button
     gui.blit(menuButton, (width/2-100, height/4*3+5))
