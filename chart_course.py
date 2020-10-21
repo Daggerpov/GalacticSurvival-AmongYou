4ad3da2fc30cbb356f7f36fa99620b0b7743818e
Daggerpov <danielagapov1@gmail.com>
2020-10-21 12:14:53 -0400

    adding comments

--- chart_course.py
+++ chart_course.py
@@ -3,20 +3,23 @@ def chart_course():
     import random 
     from main import pygame, gui, width, height, mouse, buttonFont, posText, fails
 
+    #this is done instead of the menus' space background, so that it's individual to this one task
     light_blue = (115, 194, 251)
-
     gui.fill(light_blue) 
+    
+    #makes the grid's individual squares, 8x4 based on our display resolution (1366x768p)
     grid(width, height, 170.75, 192)
 
     rocketPos = [main.rocketx, main.rockety]
     
-    '''for i in main.path:
-        pygame.draw.rect(gui, (255, 0, 0), [main.path[i][0], main.path[i][1], 170.75, 192])'''
-
-    for event in pygame.event.get(): #Allows events/actions from mouse/keyboard
+    #allows events/actions from mouse/keyboard
+    for event in pygame.event.get():
+        #if you hit command/control + Q or just quit out of the program by clicking
         if event.type == pygame.QUIT:
            quit()
+        #key presses
         if event.type == pygame.KEYDOWN:
+            #arrow keys
             if event.key == pygame.K_LEFT:
                 main.rocketx -= 170.75
                 main.angle = 270
@@ -30,28 +33,36 @@ def chart_course():
                 main.rockety += 192
                 main.angle = 180
 
+    #this determines whether the rocket has reached the end goal position
     if rocketPos == main.endPos:
+        #this will redirect to the long task when done if there is one, otherwise it'll head to the end screen
         if main.long_tasks == 1:
             main.screen = 4
         else:
             main.game_end = True
             main.screen = 5
 
+    #this adds a fail to be subtracted by the retry attempts when rocket crashes into wall
     if rocketPos[1] > 576 or rocketPos[1] < 0 or rocketPos[0] > 1195.25 or rocketPos[0] < 0:
         main.fails += 1
         main.screen = 3
 
+    #this generates the sprite image to be the rocket
     rocket = reSizeRotate(pygame.image.load("./images/rocket.png"), 171, 192, main.angle)
 
+    #draw the end goal square as red
     pygame.draw.rect(gui, (255, 0, 0), [main.endPos[0], main.endPos[1], 170.75, 192])
 
+    #displays rocket at current coordinates
     gui.blit(rocket, (main.rocketx, main.rockety))
 
+#rotates the rocket when hitting other arrow keys
 def reSizeRotate(image, width, height, angle):
     from main import pygame
     new_image = pygame.transform.rotate(pygame.transform.scale(image, (width, height)), angle)
     return new_image
 
+#generates the whole grid to be displayed
 def grid(width, height, image_width, image_height):
     import main
     from main import pygame
--- chart_course_exp.py
+++ chart_course_exp.py
@@ -7,31 +7,37 @@ def chart_course_exp():
     green = (0, 255, 0)
     blue = (0, 0, 128)
 
+    #required for all space backgrounds to be black, then have the snowfall type animation for stars
     gui.fill((0, 0, 0))
     spacebg()
 
+    #rendering text for button with anti-aliasing that has blue text
     proceedButton = buttonFont.render("Proceed", True, blue)
     
-    retryAmount = titleFont.render(f"Retries: {retries}", True, green)
+    #rendering text for the retry amount by incorporating the retry variable
+    retryAmount = titleFont.render(f"Retries: {retries}", True, green) 
 
-    for event in pygame.event.get(): #Allows events/actions from mouse/keyboard
+    #allows events/actions from keyboard and mouse to be registered
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
            quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if width//2-50 <= mouse[0] <= width//2+70 and height//1.25-20 <= mouse[1] <= height//1.25+20:
                 main.screen = 3.5
     
+    #renders all the text, anti-aliasing is ON (True) and it's dark green
     text1 = instructFont.render('Chart the course of our ship to make sure we don\'t get lost!', True, dark_green)
     text2 = instructFont.render('Win: Successfully go through all the directions from start to end', True, dark_green)
     text3 = instructFont.render('Lose Retry: By moving to the wrong direction', True, dark_green)
     text4 = instructFont.render(f'You have 10 seconds for this task, {username}', True, dark_green)
 
-
+    #displays the text for explanation
     gui.blit(text1, posText(text1, width//2, height//2-100))
     gui.blit(text2, posText(text2, width//2, height//2-50))
     gui.blit(text3, posText(text3, width//2, height//2))
     gui.blit(text4, posText(text3, width//2, height//2+50))
 
+    #lights up if hovering over proceed button
     if width//2-50 <= mouse[0] <= width//2+70 and height//1.25-20 <= mouse[1] <= height//1.25+20:
         pygame.draw.rect(gui, green, [width//2-50, height//1.25-20, 120, 40])
     else:
@@ -40,4 +46,5 @@ def chart_course_exp():
     #proceed button
     gui.blit(proceedButton, posText(proceedButton, width//2+10, height//1.25))
 
+    #retry text display
     gui.blit(retryAmount, posText(retryAmount, width//2, 75))
