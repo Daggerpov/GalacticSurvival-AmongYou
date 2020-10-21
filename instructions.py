418b686098c22df713ed3919e81f7d18443731bf
Daggerpov <danielagapov1@gmail.com>
2020-10-21 12:20:28 -0400

    Update instructions.py

--- instructions.py
+++ instructions.py
@@ -7,18 +7,24 @@ def instruct():
     green = (0, 255, 0)
     blue = (0, 0, 128)
 
+    #menu space background
     gui.fill((0, 0, 0))
     spacebg()
     
+    #rendering the text for back button as blue and with anti-aliasing ON
     back = buttonFont.render("Back", True, blue)
 
-    for event in pygame.event.get(): #Allows events/actions from mouse/keyboard
+    #allows events/actions from mouse/keyboard
+    for event in pygame.event.get(): 
+        #quit command whether you click or hit control/command + Q
         if event.type == pygame.QUIT:
            quit()
+        #mouse click on button will send to main menu
         if event.type == pygame.MOUSEBUTTONDOWN:
             if 10 <= mouse[0] <= 80 and 10 <= mouse[1] <= 50:
                 main.screen = 0
 
+    #rendering all dark green text with anti aliasing ON for the instructions
     text1 = instructFont.render('Once you click the "Setup Game" button in the main menu, you\'ll be', True, dark_green)
     text2 = instructFont.render('given a choice of how many short/long tasks you would like you do.', True, dark_green)
     text3 = instructFont.render('Each task will have a short explanation before it to tell you what', True, dark_green)
@@ -29,6 +35,7 @@ def instruct():
     text8 = instructFont.render('task, the retry bar goes down 1. The game is won by completing all', True, dark_green)
     text9 = instructFont.render('tasks and lost by running out of retries. Good luck!', True, dark_green)
 
+    #displaying that text on different 'lines'
     gui.blit(text1, posText(text1, width//2, height//2-180))
     gui.blit(text2, posText(text2, width//2, height//2-140))
     gui.blit(text3, posText(text3, width//2, height//2-100))
